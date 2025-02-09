9758c4dff5a121ce8bea9b2034867b5877062d25
Timo Koch <timokoch@uio.no>
2025-02-09 22:45:21 +0100

    Prototype erosion model

---
+++ app/main.cc
@@ -0,0 +1,340 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © Timo Koch
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+#include <config.h>
+#include <type_traits>
+#include <random>
+
+#include <dumux/common/initialize.hh>
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/numeqvector.hh>
+#include <dumux/common/boundarytypes.hh>
+#include <dumux/common/fvproblem.hh>
+
+#include <dumux/discretization/box.hh>
+#include <dumux/linear/linearsolvertraits.hh>
+#include <dumux/linear/linearalgebratraits.hh>
+#include <dumux/linear/istlsolvers.hh>
+#include <dumux/nonlinear/newtonsolver.hh>
+#include <dumux/assembly/fvassembler.hh>
+
+#include <dune/grid/yaspgrid.hh>
+#include <dumux/io/chrono.hh>
+#include <dumux/io/vtkoutputmodule.hh>
+#include <dumux/io/grid/gridmanager_yasp.hh>
+
+#include "model.hh"
+
+namespace Dumux {
+template<class TypeTag>
+class PorousMediaErosionTestProblem : public FVProblem<TypeTag>
+{
+    using ParentType = FVProblem<TypeTag>;
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    using FVElementGeometry = typename GridGeometry::LocalView;
+    using SubControlVolume = typename GridGeometry::SubControlVolume;
+    using GridView = typename GetPropType<TypeTag, Properties::GridGeometry>::GridView;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using PrimaryVariables = GetPropType<TypeTag, Properties::PrimaryVariables>;
+    using NumEqVector = Dumux::NumEqVector<PrimaryVariables>;
+    using BoundaryTypes = Dumux::BoundaryTypes<GetPropType<TypeTag, Properties::ModelTraits>::numEq()>;
+    using Indices = typename GetPropType<TypeTag, Properties::ModelTraits>::Indices;
+
+public:
+    PorousMediaErosionTestProblem(std::shared_ptr<const GridGeometry> gridGeometry)
+    : ParentType(gridGeometry)
+    {
+        // boundary conditions
+        rampUpTime_ = Chrono::toSeconds(getParam("BoundaryConditions.RampUpTime")).count();
+        const auto flowRate = getParam<Scalar>("BoundaryConditions.FlowRate");
+        const bool evaporation = getParam<bool>("BoundaryConditions.Evaporation");
+        const auto xLength = this->gridGeometry().bBoxMax()[0] - this->gridGeometry().bBoxMin()[0];
+        const auto yLength = this->gridGeometry().bBoxMax()[1] - this->gridGeometry().bBoxMin()[1];
+
+        const auto inflowLength = getParam<Scalar>("BoundaryConditions.InflowLength");
+        const auto outflowLength = getParam<Scalar>("BoundaryConditions.OutflowLength");
+
+        inflowRatePerLength_ = flowRate/inflowLength;
+        if (inflowLength < xLength)
+        {
+            inflowBottom_ = 0.5*inflowLength;
+            inflowSide_ = 0.0;
+        }
+        else
+        {
+            inflowBottom_ = 0.5*xLength;
+            inflowSide_ = 0.5*(inflowLength - xLength);
+        }
+
+        outflowRatePerLength_ = evaporation ? 0.0 : flowRate/outflowLength;
+        if (outflowLength < xLength)
+        {
+            outflowBottom_ = 0.5*outflowLength;
+            outflowSide_ = 0.0;
+        }
+        else
+        {
+            outflowBottom_ = 0.5*xLength;
+            outflowSide_ = 0.5*(outflowLength - xLength);
+        }
+
+        evapRatePerArea_ = evaporation ? flowRate/(yLength*xLength) : 0.0;
+
+        if (this->gridGeometry().gridView().comm().rank() == 0)
+        {
+            std::cout << "\nBoundary conditions:\n";
+            std::cout << "-- inflow: " << inflowRatePerLength_*inflowLength << "\n";
+            std::cout << "-- outflow: " << outflowRatePerLength_*outflowLength << "\n";
+            std::cout << "-- evaporation: " << evapRatePerArea_*yLength*xLength << "\n";
+        }
+
+        if (inflowRatePerLength_*inflowLength - outflowRatePerLength_*outflowLength - evapRatePerArea_*yLength*xLength > 1e-6)
+            DUNE_THROW(Dune::Exception, "Mass balance in the boundary conditions not satisfied.");
+
+        // model parameters
+        fluidCompressibility_ = getParam<Scalar>("ModelParameters.FluidCompressibility");
+        fieldRelaxationRate_ = getParam<Scalar>("ModelParameters.FieldRelaxationRate");
+        solidDiffusivity_ = getParam<Scalar>("ModelParameters.SolidDiffusivity");
+        fieldDiffusivity_ = getParam<Scalar>("ModelParameters.FieldDiffusivity");
+    }
+
+    BoundaryTypes boundaryTypesAtPos(const GlobalPosition& globalPos) const
+    {
+        BoundaryTypes values;
+        values.setAllNeumann();
+        return values;
+    }
+
+    NumEqVector neumannAtPos(const GlobalPosition& globalPos) const
+    {
+        NumEqVector values(0.0);
+
+        if (isIntializationPhase_)
+            return values;
+
+        const auto rampFactor = std::min(1.0, 1.0/rampUpTime_*time_);
+        if (globalPos[1] < this->gridGeometry().bBoxMin()[1] + 1e-6 && globalPos[0] < inflowBottom_ && globalPos[0] > -inflowBottom_)
+            values[0] = -inflowRatePerLength_*rampFactor;
+        else if (onSide_(globalPos) && globalPos[1] < this->gridGeometry().bBoxMin()[1] + inflowSide_)
+            values[0] = -inflowRatePerLength_*rampFactor;
+        else if (globalPos[1] > this->gridGeometry().bBoxMax()[1] - 1e-6)
+            values[0] = outflowRatePerLength_*rampFactor;
+        else if (onSide_(globalPos) && globalPos[1] > this->gridGeometry().bBoxMax()[1] - outflowSide_)
+            values[0] = outflowRatePerLength_*rampFactor;
+
+        return values;
+    }
+
+    NumEqVector sourceAtPos(const GlobalPosition& globalPos) const
+    {
+        if (isIntializationPhase_)
+            return { 0.0, 0.0, 0.0 };
+
+        const auto rampFactor = std::min(1.0, 1.0/rampUpTime_*time_);
+        return { -evapRatePerArea_*rampFactor, 0.0, 0.0 };
+    }
+
+    Scalar fluidCompressibility() const { return fluidCompressibility_; }
+    Scalar fieldRelaxationRate() const { return fieldRelaxationRate_; }
+    Scalar solidDiffusivity() const { return solidDiffusivity_; }
+    Scalar fieldDiffusivity() const { return fieldDiffusivity_; }
+
+    void stopInitializationPhase() { isIntializationPhase_ = false; }
+    void startInitializationPhase() { isIntializationPhase_ = true; }
+
+    void setSolidDiffusivity(const Scalar D) { solidDiffusivity_ = D; }
+    void setTime(Scalar time) { time_ = time; }
+
+private:
+    // boundary conditions
+    bool onSide_(const GlobalPosition& globalPos) const
+    {
+        return globalPos[0] < this->gridGeometry().bBoxMin()[0] + 1e-6 ||
+               globalPos[0] > this->gridGeometry().bBoxMax()[0] - 1e-6;
+    }
+    Scalar time_ = 0.0;
+    Scalar rampUpTime_;
+    Scalar inflowRatePerLength_, inflowBottom_, inflowSide_;
+    Scalar outflowRatePerLength_, outflowBottom_, outflowSide_;
+    Scalar evapRatePerArea_;
+
+    // model parameters
+    Scalar fluidCompressibility_;
+    Scalar fieldRelaxationRate_;
+    Scalar solidDiffusivity_;
+    Scalar fieldDiffusivity_;
+
+    bool isIntializationPhase_ = true;
+};
+
+} // end namespace Dumux
+
+namespace Dumux::Properties::TTag {
+struct PorousMediaErosionTest
+{
+    using InheritsFrom = std::tuple<PorousMediaErosionModel, BoxModel>;
+
+    using Grid = Dune::YaspGrid<2, Dune::EquidistantOffsetCoordinates<double, 2>>;
+
+    template<class TypeTag>
+    using Problem = PorousMediaErosionTestProblem<TypeTag>;
+
+    using EnableGridGeometryCache = std::true_type;
+    using EnableGridVolumeVariablesCache = std::true_type;
+    using EnableGridFluxVariablesCache = std::true_type;
+};
+} // end namespace Dumux::Properties
+
+// functor for data communication with MPI
+struct MinScatter
+{
+    template<class A, class B>
+    static void apply(A& a, const B& b)
+    { a[1] = std::min(a[1], b[1]); }
+};
+
+// create the random initial solution
+template<class SolutionVector, class GridGeometry>
+SolutionVector createInitialSolution(const GridGeometry& gg)
+{
+    SolutionVector sol(gg.numDofs());
+
+    // Generate random number and add processor offset
+    // For sequential runs `rank` always returns `0`.
+    std::mt19937 gen(0); // seed is 0 for deterministic results
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+    for (int n = 0; n < sol.size(); ++n)
+    {
+        sol[n][0] = 0.0;
+        sol[n][1] = 0.8 + 0.1*(0.5-dis(gen)) + gg.gridView().comm().rank();
+        sol[n][2] = 0.8;
+    }
+
+    // We take the value of the processor with the minimum rank
+    // and subtract the rank offset
+    if (gg.gridView().comm().size() > 1)
+    {
+        Dumux::VectorCommDataHandle<
+            typename GridGeometry::VertexMapper,
+            SolutionVector,
+            GridGeometry::GridView::dimension,
+            MinScatter
+        > minHandle(gg.vertexMapper(), sol);
+        gg.gridView().communicate(minHandle, Dune::All_All_Interface, Dune::ForwardCommunication);
+
+        // Remove processor offset
+        for (int n = 0; n < sol.size(); ++n)
+            sol[n][1] -= std::floor(sol[n][1]);
+    }
+    return sol;
+}
+
+int main(int argc, char** argv)
+{
+    using namespace Dumux;
+
+    Dumux::initialize(argc, argv);
+    Parameters::init(argc, argv);
+
+    using TypeTag = Properties::TTag::PorousMediaErosionTest;
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using Grid = GetPropType<TypeTag, Properties::Grid>;
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    using Problem = GetPropType<TypeTag, Properties::Problem>;
+    using SolutionVector = GetPropType<TypeTag, Properties::SolutionVector>;
+    using GridVariables = GetPropType<TypeTag, Properties::GridVariables>;
+
+    GridManager<Grid> gridManager;
+    gridManager.init();
+    const auto rank = gridManager.grid().leafGridView().comm().rank();
+
+    auto gridGeometry = std::make_shared<GridGeometry>(gridManager.grid().leafGridView());
+    auto problem = std::make_shared<Problem>(gridGeometry);
+    auto sol = createInitialSolution<SolutionVector>(*gridGeometry);
+    auto gridVariables = std::make_shared<GridVariables>(problem, gridGeometry);
+    gridVariables->init(sol);
+
+    VtkOutputModule<GridVariables, SolutionVector> vtkWriter(*gridVariables, sol, problem->name());
+    vtkWriter.addVolumeVariable([](const auto& vv){ return vv.pressure(); }, "p");
+    vtkWriter.addVolumeVariable([](const auto& vv){ return vv.solidity(); }, "phi");
+    vtkWriter.addVolumeVariable([](const auto& vv){ return vv.g(); }, "g");
+    vtkWriter.write(-1.0);
+
+    auto timeLoop = std::make_shared<CheckPointTimeLoop<Scalar>>(
+        Chrono::toSeconds("-1.0s"), Chrono::toSeconds("0.1s"), Chrono::toSeconds(getParam("TimeLoop.TEnd"))
+    );
+    timeLoop->setMaxTimeStepSize(0.1);
+
+    using Assembler = FVAssembler<TypeTag, DiffMethod::numeric>;
+    using LinearSolver = AMGBiCGSTABIstlSolver<LinearSolverTraits<GridGeometry>, LinearAlgebraTraitsFromAssembler<Assembler>>;
+    using Solver = Dumux::NewtonSolver<Assembler, LinearSolver>;
+
+    auto oldSol = sol;
+    auto assembler = std::make_shared<Assembler>(problem, gridGeometry, gridVariables, timeLoop, oldSol);
+    auto linearSolver = std::make_shared<LinearSolver>(gridGeometry->gridView(), gridGeometry->dofMapper());
+    Solver solver(assembler, linearSolver);
+
+    // initialization phase idea: we start with a random field and then diffusive such that we get
+    // a Gaussian random field for the solidity to start with
+    const auto correlationLength = getParam<Scalar>("ModelParameters.CorrelationLength");
+    const auto D = correlationLength*correlationLength/4.0; // Running diffusion for 1 time unit
+    problem->startInitializationPhase();
+    problem->setSolidDiffusivity(D);
+
+    if (rank == 0)
+        std::cout << "\nStarting initialization phase with solid diffusivity " << problem->solidDiffusivity() << "\n" << std::endl;
+
+    timeLoop->start(); do {
+        problem->setTime(timeLoop->time() + timeLoop->timeStepSize());
+        solver.solve(sol, *timeLoop);
+        oldSol = sol;
+        gridVariables->advanceTimeStep();
+        timeLoop->advanceTimeStep();
+        if (timeLoop->timeStepIndex() != 10)
+            vtkWriter.write(timeLoop->time());
+        timeLoop->reportTimeStep();
+    } while (timeLoop->timeStepIndex() < 10);
+
+    // re-initialize timeloop and problem
+    timeLoop->setTime(0.0);
+    timeLoop->setTimeStepSize(Chrono::toSeconds(getParam("TimeLoop.InitialTimeStepSize")));
+    timeLoop->setMaxTimeStepSize(Chrono::toSeconds(getParam("TimeLoop.MaxTimeStepSize")));
+    problem->stopInitializationPhase();
+    problem->setSolidDiffusivity(getParam<Scalar>("ModelParameters.SolidDiffusivity", 0.0));
+
+    // re-initialize solution
+    for (int n = 0; n < sol.size(); ++n)
+    {
+        sol[n][0] = 0.0;
+        sol[n][2] = 0.8;
+    }
+
+    oldSol = sol;
+    gridVariables->init(sol);
+    vtkWriter.write(0.0);
+
+    if (rank == 0)
+        std::cout << "\nStarting main phase with solid diffusivity " << problem->solidDiffusivity() << "\n" << std::endl;
+
+    do {
+        problem->setTime(timeLoop->time() + timeLoop->timeStepSize());
+        solver.solve(sol, *timeLoop);
+        oldSol = sol;
+        gridVariables->advanceTimeStep();
+        timeLoop->advanceTimeStep();
+        vtkWriter.write(timeLoop->time());
+        timeLoop->reportTimeStep();
+        timeLoop->setTimeStepSize(solver.suggestTimeStepSize(timeLoop->timeStepSize()));
+    } while (!timeLoop->finished());
+
+    // print the final report
+    timeLoop->finalize(gridGeometry->gridView().comm());
+    return 0;
+}
---
+++ app/model.hh
@@ -0,0 +1,241 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © Timo Koch
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+
+#ifndef DUMUX_POROUS_MEDIA_EROSION_MODEL_HH
+#define DUMUX_POROUS_MEDIA_EROSION_MODEL_HH
+
+#include <dune/common/fvector.hh>
+#include <dumux/common/math.hh>
+#include <dumux/common/properties.hh>
+#include <dumux/common/numeqvector.hh>
+#include <dumux/discretization/method.hh>
+#include <dumux/discretization/evalgradients.hh>
+
+namespace Dumux::Properties::TTag {
+struct PorousMediaErosionModel {};
+} // end namespace Dumux::Properties::TTag
+
+namespace Dumux {
+
+// variables
+template <class Traits>
+class PorousMediaErosionModelVolumeVariables
+{
+    using Scalar = typename Traits::PrimaryVariables::value_type;
+    static_assert(Traits::PrimaryVariables::dimension == Traits::ModelTraits::numEq());
+public:
+    using PrimaryVariables = typename Traits::PrimaryVariables;
+    using Indices = typename Traits::ModelTraits::Indices;
+
+    template<class ElementSolution, class Problem, class Element, class SubControlVolume>
+    void update(const ElementSolution& elemSol,
+                const Problem& problem,
+                const Element& element,
+                const SubControlVolume& scv)
+    {
+        priVars_ = elemSol[scv.indexInElement()];
+        const auto solidity = priVars_[Indices::solidityIdx];
+        const auto porosity = 1.0 - solidity;
+        permeability_ = porosity*porosity*porosity/(solidity*solidity); // Kozeny-Carman
+    }
+
+    Scalar pressure() const
+    { return priVars_[Indices::pressureIdx]; }
+
+    Scalar solidity() const
+    { return priVars_[Indices::solidityIdx]; }
+
+    Scalar g() const
+    { return priVars_[Indices::gIdx]; }
+
+    Scalar permeability() const
+    { return permeability_; }
+
+    Scalar priVar(const int pvIdx) const
+    { return priVars_[pvIdx]; }
+
+    const PrimaryVariables& priVars() const
+    { return priVars_; }
+
+    Scalar extrusionFactor() const
+    { return 1.0; }
+
+private:
+    PrimaryVariables priVars_;
+    Scalar permeability_;
+};
+} // end namespace Dumux
+
+
+// equations
+namespace Dumux {
+template<class TypeTag>
+class PorousMediaErosionModelLocalResidual
+: public GetPropType<TypeTag, Properties::BaseLocalResidual>
+{
+    using ParentType = GetPropType<TypeTag, Properties::BaseLocalResidual>;
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using Problem = GetPropType<TypeTag, Properties::Problem>;
+    using NumEqVector = Dumux::NumEqVector<GetPropType<TypeTag, Properties::PrimaryVariables>>;
+
+    using GridVariables = GetPropType<TypeTag, Properties::GridVariables>;
+    using VolumeVariables = typename GridVariables::GridVolumeVariables::VolumeVariables;
+    using ElementVolumeVariables = typename GridVariables::GridVolumeVariables::LocalView;
+    using ElementFluxVariablesCache = typename GridVariables::GridFluxVariablesCache::LocalView;
+
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    using FVElementGeometry = typename GridGeometry::LocalView;
+    using SubControlVolume = typename GridGeometry::SubControlVolume;
+    using SubControlVolumeFace = typename GridGeometry::SubControlVolumeFace;
+    using GridView = typename GridGeometry::GridView;
+    using Element = typename GridView::template Codim<0>::Entity;
+
+    using ModelTraits = GetPropType<TypeTag, Properties::ModelTraits>;
+    using Indices = typename ModelTraits::Indices;
+    static constexpr int dimWorld = GridView::dimensionworld;
+public:
+    using ParentType::ParentType;
+
+    NumEqVector computeStorage(const Problem& problem,
+                               const SubControlVolume& scv,
+                               const VolumeVariables& volVars) const
+    {
+        NumEqVector storage;
+        const auto kappa = problem.fluidCompressibility();
+        const auto gRelaxationRate = problem.fieldRelaxationRate();
+        storage[Indices::massBalanceEqIdx] = kappa*volVars.pressure();
+        storage[Indices::solidityEqIdx] = volVars.solidity();
+        storage[Indices::blurEqIdx] = volVars.g()*gRelaxationRate;
+        return storage;
+    }
+
+    NumEqVector computeFlux(const Problem& problem,
+                            const Element& element,
+                            const FVElementGeometry& fvGeometry,
+                            const ElementVolumeVariables& elemVolVars,
+                            const SubControlVolumeFace& scvf,
+                            const ElementFluxVariablesCache& elemFluxVarsCache) const
+    {
+        static_assert(DiscretizationMethods::isCVFE<typename GridGeometry::DiscretizationMethod>,
+            "This local residual is hard-coded to control-volume finite element schemes");
+
+        const auto& fluxVarCache = elemFluxVarsCache[scvf];
+        Dune::FieldVector<Scalar, dimWorld> gradPressure(0.0);
+        Dune::FieldVector<Scalar, dimWorld> gradG(0.0);
+        Scalar permeability = 0.0;
+        const auto& shapeValues = fluxVarCache.shapeValues();
+        for (const auto& scv : scvs(fvGeometry))
+        {
+            const auto& volVars = elemVolVars[scv];
+            gradPressure.axpy(volVars.pressure(), fluxVarCache.gradN(scv.indexInElement()));
+            gradG.axpy(volVars.g(), fluxVarCache.gradN(scv.indexInElement()));
+            permeability += volVars.permeability()*shapeValues[scv.indexInElement()][0];
+        }
+
+        NumEqVector flux;
+        flux[Indices::massBalanceEqIdx] = -1.0*vtmv(scvf.unitOuterNormal(), permeability, gradPressure)*scvf.area();
+        const auto B = problem.fieldDiffusivity();
+        flux[Indices::blurEqIdx] = -1.0*vtmv(scvf.unitOuterNormal(), B, gradG)*scvf.area();
+
+        const auto D = problem.solidDiffusivity();
+        if (D > 0.0)
+        {
+            Dune::FieldVector<Scalar, dimWorld> gradSolidity(0.0);
+            for (const auto& scv : scvs(fvGeometry))
+                gradSolidity.axpy(elemVolVars[scv].solidity(), fluxVarCache.gradN(scv.indexInElement()));
+            flux[Indices::solidityEqIdx] = -1.0*vtmv(scvf.unitOuterNormal(), D, gradSolidity)*scvf.area();
+        }
+
+        return flux;
+    }
+
+    NumEqVector computeSource(const Problem& problem,
+                              const Element& element,
+                              const FVElementGeometry& fvGeometry,
+                              const ElementVolumeVariables& elemVolVars,
+                              const SubControlVolume& scv) const
+    {
+        NumEqVector source(0.0);
+        source[Indices::massBalanceEqIdx] = 0.0;
+        const auto& volVars = elemVolVars[scv];
+        static const Scalar omega = getParam<Scalar>("ModelParameters.Omega");
+        static const Scalar phiStar = getParam<Scalar>("ModelParameters.PhiStar");
+        static const auto H = [&](const Scalar phi) { return 0.5*(1.0 + std::tanh(omega*(phi - phiStar))); };
+        static const Scalar H0 = H(0.0);
+        static const Scalar H1 = H(1.0);
+        const Scalar psi = (H(volVars.g())-H0)/(H1 - H0);
+
+        const auto geometry = element.geometry();
+        const auto elemSol = elementSolution(element, elemVolVars, fvGeometry);
+        const auto gradP = evalGradients(element, geometry, fvGeometry.gridGeometry(), elemSol, scv.dofPosition())[Indices::pressureIdx];
+        const auto pSquared = gradP*gradP;
+
+        source[Indices::solidityEqIdx] = -volVars.solidity()*std::max(0.0, pSquared - psi);
+
+        source[Indices::blurEqIdx] = volVars.solidity() - volVars.g();
+        source += problem.source(element, fvGeometry, elemVolVars, scv);
+        return source;
+    }
+};
+} // end namespace Dumux
+
+
+// configure the PorousMediaErosionModel
+namespace Dumux::Properties {
+
+template<class TypeTag>
+struct LocalResidual<TypeTag, TTag::PorousMediaErosionModel>
+{ using type = PorousMediaErosionModelLocalResidual<TypeTag>; };
+
+template<class TypeTag>
+struct Scalar<TypeTag, TTag::PorousMediaErosionModel>
+{ using type = double; };
+
+template<class TypeTag>
+struct ModelTraits<TypeTag, TTag::PorousMediaErosionModel>
+{
+    struct type
+    {
+        struct Indices
+        {
+            static constexpr int pressureIdx = 0;
+            static constexpr int solidityIdx = 1;
+            static constexpr int gIdx = 2;
+
+            static constexpr int massBalanceEqIdx = 0;
+            static constexpr int solidityEqIdx = 1;
+            static constexpr int blurEqIdx = 2;
+        };
+
+        static constexpr int numEq() { return 3; }
+    };
+};
+
+template<class TypeTag>
+struct PrimaryVariables<TypeTag, TTag::PorousMediaErosionModel>
+{
+    using type = Dune::FieldVector<
+        GetPropType<TypeTag, Properties::Scalar>,
+        GetPropType<TypeTag, Properties::ModelTraits>::numEq()
+    >;
+};
+
+template<class TypeTag>
+struct VolumeVariables<TypeTag, TTag::PorousMediaErosionModel>
+{
+    struct Traits
+    {
+        using PrimaryVariables = GetPropType<TypeTag, Properties::PrimaryVariables>;
+        using ModelTraits = GetPropType<TypeTag, Properties::ModelTraits>;
+    };
+
+    using type = PorousMediaErosionModelVolumeVariables<Traits>;
+};
+
+} // end namespace Dumux::Properties
+
+#endif
